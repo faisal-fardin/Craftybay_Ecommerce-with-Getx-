27a8aac82a710426d830284f110e2812c61b1023
Your Name <faisalfardin77@gmail.com>
2024-04-07 17:35:21 +0600

    Product Controler add

--- lib/presentation/state_holders/product_controller.dart
+++ lib/presentation/state_holders/product_controller.dart
@@ -3,8 +3,18 @@ import 'package:get/get.dart';
 
 class ProductController extends GetxController{
 
-  bool getPopularProductInProgress = false;
+  bool _getPopularProductInProgress = false;
+
   ProductModel _productModel = ProductModel();
+
+
+  ProductModel get categoryModel => _productModel;
+  bool get getPopularProductInProgress => _getPopularProductInProgress;
+
+
+  Future getProduct() async{
+
+  }
 
 
 
